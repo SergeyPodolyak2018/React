f47d54cd28767383da459b409ae2e923b128ad85
SergeyPodolyak <sergey.podolyak2018@gmail.com>
2018-02-22 22:59:01 +0200

    Coneckt to test api yahooooooo

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -2,8 +2,8 @@ import React, {Component} from 'react'
 //import React from 'react'
 
 import ArticleList from './ArticleList'
-import articles from '../fixtures'
-//import data from '../otherAPI'
+//import articles from '../fixtures'
+
 import 'bootstrap/dist/css/bootstrap.css'
 
 class App extends Component{
@@ -16,40 +16,29 @@ class App extends Component{
 			hits: []
 		}
 	}
-/*
+
 componentDidMount() {
-	console.log('herhehr',1)
+	console.log("class App -- componentDidMount",2)
     var xhr = new XMLHttpRequest();
     var status = false;
-    xhr.open("GET", "http://www.mocky.io/v2/5a8ea0182f0000113e4f2775", true);
-    xhr.onload = function (e) {
-      if (xhr.readyState === 4) {
+    xhr.open("GET", "http://www.mocky.io/v2/5a8f252b3000004900248a49", true);
+    xhr.onload = function (e){
+    	if (xhr.readyState === 4) {
         if (xhr.status === 200) {
+        	console.log(JSON.parse(xhr.responseText));
 
-        	console.log(xhr.responseText);
-        	console.log(alfha);
-        	
-        	this.insertData.bind(this,xhr.responseText);
-          //this.setState({ data: xhr.responseText });
+        	this.setState({data:JSON.parse(xhr.responseText)});
           status = true;
         } else {
           console.error(xhr.statusText);
         }
       }
-    };
+    }.bind(this);
     xhr.onerror = function (e) {
       console.error(xhr.statusText);
     };
     xhr.send(null);
   }
-*/
-
-
-componentDidMount() {
-	fetch("http://www.mocky.io/v2/5a8ea0182f0000113e4f2775")
-      .then(response => {console.log(response);response.json()}).then(data => this.setState({ hits: data.hits, isLoading: false }));
-  
-}
 
 
 
@@ -57,41 +46,43 @@ componentDidMount() {
 
 	render(){
 			console.log("class App -- render",1)
-			console.log(this.data)
 			
-
-			return(
-				
-				<div className='container'>
-				<div className='jumbotron'>
-					<h1 className='display-3'>
-						App name
-						<button className="btn" onClick={this.reversed}>Реверс</button>
-					</h1>
-					</div>
-						<ArticleList articles={this.state.reverted? articles.slice().reverse() : articles} />
-					</div>
-				
-			)
+			if(this.state.data===null){
+				return(
+						<div className='container'>
+						<div className='jumbotron'>
+							<h1 className='display-3'>
+								App name								
+							</h1>
+						</div>
+							<h3>Загрузка</h3>
+						</div>
+					)
+			}else{
+				return(
+					
+					<div className='container'>
+					<div className='jumbotron'>
+						<h1 className='display-3'>
+							App name
+							<button className="btn" onClick={this.reversed}>Реверс</button>
+						</h1>
+						</div>
+							<ArticleList articles={this.state.reverted? this.state.data.slice().reverse() : this.state.data} />
+						</div>
+					
+					)
+				}
 		}
-
-
-		insertData=(piska)=>{
-			console.log(this)
-			this.setState({
-				data:piska
-			})
-			
-
-		}
+	
 
 
 		reversed=()=>{
-			console.log(this)
+			console.log("class App -- reversed",3)
 			this.setState({
 				reverted:!this.state.reverted
 			})
-			console.log('',this.state.reverted)
+			
 
 		}
 	
